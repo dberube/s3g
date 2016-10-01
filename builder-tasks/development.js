6405489c9a2687561d81e9e1c5384f620a4311da
David Berube <d@berube.co>
2016-10-01 04:06:47 -0400

    Small bug fixes

--- builder-tasks/development.js
+++ builder-tasks/development.js
@@ -1,4 +1,5 @@
 module.exports = function( gulp, cb ) {
 	gulp.p.util.log('Development', 'Starting development watch and server', gulp.p.util.colors.yellow( 'START' ));
-	gulp.p.sequence( 'clean:development', 'static', 'bower', [ 'styles', 'views' ], 'clean:vendors', [ 'server', 'watch' ] )(cb);
+
+	gulp.p.sequence( 'clean:development', 'static', 'bower', [ 'styles', 'views' ], [ 'server', 'watch' ] )(cb);
 }
